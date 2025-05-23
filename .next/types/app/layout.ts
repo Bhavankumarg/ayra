3ae47261774938899c2f14483eca592f7ae65ec4
Bhavankumarg <gbhavanbkr@gmail.com>
2025-05-23 12:37:38 +0530

    port changes

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/bhavankumarganesan/iVistaz-Github/May/ayra-new/app/layout.js
+// File: /Users/bhavankumarganesan/iVistaz-Github/May/ayra/app/layout.js
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
