08f7191c9e95a7d0519636d5097693066fe198ee
Bhavankumarg <gbhavanbkr@gmail.com>
2025-05-23 15:24:10 +0530

    google site verification added

--- app/layout.js
+++ app/layout.js
@@ -41,9 +41,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="index, follow" />
+        <meta name="google-site-verification" content="googled87b13e1b76e2421.html" />
         <link rel="canonical" href="https://ayra.education" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
 
